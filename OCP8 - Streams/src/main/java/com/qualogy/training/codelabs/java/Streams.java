6c74a79e6b3037c095de3bb9b1e560e5ede7b3f3
Arnout Wind <arnout.wind@cobase.com>
2019-03-11 18:03:05 +0100

    updated the answers to the latest codelabs

--- OCP8 - Streams/src/main/java/com/qualogy/training/codelabs/java/Streams.java
+++ OCP8 - Streams/src/main/java/com/qualogy/training/codelabs/java/Streams.java
@@ -2,8 +2,6 @@ package com.qualogy.training.codelabs.java;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,7 +12,6 @@ public class Streams {
     * This method gets a collection and expects a stream to be returned
     **/
     public Stream collection_to_stream(Collection collection){
-
         return collection.stream();
     }
 
@@ -22,7 +19,6 @@ public class Streams {
     * This method gets a stream and expects any type of collection to be returned.
     **/
     public Collection stream_to_collection(Stream stream){
-
         return (Collection)stream
                 .collect(Collectors.toCollection(ArrayList::new));
         /*
@@ -37,7 +33,6 @@ public class Streams {
      * after filtering the collection return a new collection which was filtered.
      */
     public Collection<String> filter_collection_using_stream_given_function(Predicate<String> filterFunction, Collection<String> collection){
-
         return collection
                 .stream()
                 .filter(filterFunction)
@@ -73,12 +68,83 @@ public class Streams {
     }
 
     /**
-     * This method takes a stream instead of a collection to make it more cumbersome that you might extract the collections and do .addAll on them (you can still do it but lets stick to the training
-     * You get a stream which contains Collections, merge these two collections into a single collection and return this
+     * This method takes a collection which contains Collections, merge these two collections into a single collection and return the result
      */
-    public Collection<String> merge_a_collection_of_collection_into_one_collection(Stream<Collection<String>> stream){
-        return stream
+    public Collection<String> merge_a_collection_of_collection_into_one_collection(Collection<Collection<String>> collection){
+        return collection
+                .stream()
                 .flatMap(Collection::stream)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
+     * This method takes a collection, using the stream obtain the amount of entries within the collection and return it
+     */
+    public long count_the_amount_of_entries_in_the_collection(Collection collection){
+        return collection
+                .stream()
+                .count();
+    }
+
+    /**
+     * This method takes a collection and expects you to chain what you have learned in your previous codelabs, in the previous codelab only one operation was called upon the stream, this test requires you to call multiple
+     * log to system out println upon entry, filter your stream, log the outcome of that and count the results
+     */
+    public long chain_different_stream_operations_log_filter_log_and_count_the_collection(Predicate<String> filterFunction, Collection<String> collection){
+        return collection
+                .stream()
+                .peek(System.out::println)
+                .filter(filterFunction)
+                .peek(System.out::println)
+                .count();
+    }
+
+    /**
+     * This method gets a collection containing numbers, sum all the elements in the list and return it
+     */
+    public Integer sum_all_numbers_in_the_collection(Collection<Integer> collection){
+        return collection
+                .stream()
+                .reduce(Integer::sum)
+                .get();
+    }
+
+    /**
+     * This method gets a collection which contains all kind of objects, check if it contains a String
+     */
+    public boolean validate_if_collection_contains_a_string(Collection collection){
+        return collection
+                .stream()
+                .anyMatch((e) -> e instanceof String);
+    }
+
+    /**
+     * This method gets a collection which contains only Integers, check if all integers are above 5
+     */
+    public boolean validate_if_collection_only_has_integers_above_5(Collection<Integer> collection){
+        return collection
+                .stream()
+                .allMatch((e) -> e > 5);
+    }
+
+    /**
+     * This method gets a collection of objects, in this case Strings, there are duplicates within this collection, you have to filter out the duplicates
+     * You have learned the .filter method before so try solving this codelab without using the .filter function
+     */
+    public Collection remove_all_duplicates_from_the_stream_without_using_filter(Collection collection){
+       return (Collection) collection
+               .stream()
+               .distinct()
+               .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    /**
+     * You will get a collection which has a size of 7, trim it down to 5 using streams
+     */
+    public Collection trim_the_collection_to_a_maximum_size(Collection collection){
+        return (Collection) collection
+                .stream()
+                .limit(5)
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 }
--- OCP8 - Streams/src/test/java/com/qualogy/training/codelabs/java/StreamsTest.java
+++ OCP8 - Streams/src/test/java/com/qualogy/training/codelabs/java/StreamsTest.java
@@ -1,18 +1,24 @@
 package com.qualogy.training.codelabs.java;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static java.util.Optional.empty;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class StreamsTest {
+    public static final Predicate<String> FILTER_FUNCTION = (e) -> !e.startsWith("Bad ");
+    public static final List<Integer> LIST_OF_NUMBERS = Arrays.asList(1, 2, 4, 16, 9);
     private Streams test = new Streams();
+    private PrintStream originalStream;
+    private PrintStream mockStream;
 
     private List<String> inputList;
 
@@ -26,10 +32,14 @@ public class StreamsTest {
         inputList.add("Bad Shrimp");
         inputList.add("Shiny Apple");
         inputList.add("Perfect Orange");
+
+        originalStream = System.out;
+        mockStream = mock(PrintStream.class);
+        System.setOut(mockStream);
     }
 
     @Test
-    public void extract_stream(){
+    public void collection_to_stream(){
         Stream stream = test.collection_to_stream(Collections.EMPTY_LIST);
 
         assertNotNull(stream);
@@ -37,55 +47,95 @@ public class StreamsTest {
 
 
     @Test
-    public void extract_collection(){
+    public void stream_to_collection(){
         Collection collection = test.stream_to_collection(Stream.empty());
 
         assertNotNull(collection);
     }
 
     @Test
-    public void filter_list(){
-        Collection result = test.filter_collection_using_stream_given_function((e) -> !e.startsWith("Bad "), inputList);
+    public void filter_collection_using_stream_given_function(){
+        Collection result = test.filter_collection_using_stream_given_function(FILTER_FUNCTION, inputList);
 
         assertEquals(4, result.size());
     }
 
     @Test
-    public void log_every_entry(){
-        PrintStream originalStream = System.out;
-        PrintStream mockStream = mock(PrintStream.class);
-        System.setOut(mockStream);
-
+    public void logging_every_entry_in_the_collection_using_system_out_println(){
         Collection result = test.logging_every_entry_in_the_collection_using_system_out_println(inputList);
 
         assertEquals(inputList, result);
 
         verify(mockStream, times(7)).println(any(String.class));
         verify(mockStream).println("Bad Apple");
-
-        System.setOut(originalStream);
     }
 
     @Test
-    public void string_collection_to_integer_collection(){
+    public void convert_every_entry_from_string_to_integer(){
         Collection<String> input = Arrays.asList("1", "2", "4", "16", "9");
-        Collection<Integer> expectedOutput = Arrays.asList(1, 2, 4, 16, 9);
 
         Collection<Integer> result = test.convert_every_entry_from_string_to_integer(input);
 
-        assertEquals(expectedOutput, result);
+        assertEquals(LIST_OF_NUMBERS, result);
     }
 
     @Test
-    public void merge_collections(){
+    public void merge_a_collection_of_collection_into_one_collection(){
         Collection<String> input1 = Arrays.asList("1", "2", "4", "16", "9");
         Collection<String> input2 = Arrays.asList("a", "b", "c", "d", "f");
         Collection<Collection<String>> jointInput = Arrays.asList(input1, input2);
 
-        Collection<String> result = test.merge_a_collection_of_collection_into_one_collection(jointInput.stream());
+        Collection<String> result = test.merge_a_collection_of_collection_into_one_collection(jointInput);
 
         Collection<String> expectedOutput = Arrays.asList("1", "2", "4", "16", "9", "a", "b", "c", "d", "f");
 
         assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void count_the_amount_of_entries_in_the_collection(){
+        assertEquals(7, test.count_the_amount_of_entries_in_the_collection(inputList));
+    }
+
+    @Test
+    public void chain_different_stream_operations_log_filter_log_and_count_the_collection(){
+        assertEquals(4, test.chain_different_stream_operations_log_filter_log_and_count_the_collection(FILTER_FUNCTION, inputList));
+
+        verify(mockStream, times(11)).println(any(String.class));
+        verify(mockStream).println("Bad Apple");
+    }
+
+    @Test
+    public void sum_all_numbers_in_the_collection(){
+        assertEquals(new Integer(32), test.sum_all_numbers_in_the_collection(LIST_OF_NUMBERS));
+    }
+
+    @Test
+    public void validate_if_collection_contains_a_string(){
+        Collection input = Arrays.asList(42, 2L, 0x54, false, "target string", test, empty());
+        assertTrue(test.validate_if_collection_contains_a_string(input));
+    }
+
+    @Test
+    public void validate_if_collection_only_has_integers_above_5(){
+        Collection input = Arrays.asList(6, 17, 2040592, 9);
+        assertTrue(test.validate_if_collection_only_has_integers_above_5(input));
+    }
+
+    @Test
+    public void remove_all_duplicates_from_the_stream_without_using_filter(){
+        Collection input = Arrays.asList("first", "second", "third", "first", "duplicate again", "duplicate again", "last");
+
+        assertEquals(5, test.remove_all_duplicates_from_the_stream_without_using_filter(input).size());
+    }
+
+    @Test
+    public void trim_the_collection_to_a_maximum_size(){
+        assertEquals(5, test.trim_the_collection_to_a_maximum_size(inputList).size());
+    }
+
+    @After
+    public void cleanUp(){
+        System.setOut(originalStream);
     }
 }
