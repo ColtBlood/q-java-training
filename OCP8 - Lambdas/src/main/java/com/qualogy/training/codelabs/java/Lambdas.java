0226045f6d77d69553a56b0919e3136f354c7aba
Arnout Wind <arnout.wind@cobase.com>
2019-03-12 17:29:02 +0100

    added the labs for Lambdas, missing still the answers but also the required powerpoint to give intro to this topic, and more tests are coming

---
+++ OCP8 - Lambdas/src/main/java/com/qualogy/training/codelabs/java/Lambdas.java
@@ -0,0 +1,59 @@
+package com.qualogy.training.codelabs.java;
+
+import java.util.List;
+import java.util.function.*;
+
+public class Lambdas {
+    //TODO: add answers and powerpoint!!!! && extra assignments
+    public Integer create_a_simple_method_which_doubles_the_integer_value(Integer input){
+        return 0;
+    }
+
+    public IllegalArgumentException create_a_anonymous_class_with_a_subtype_of_illegal_argument_exception(){
+        return null;
+    }
+
+    public Function<String, String> create_a_short_hand_function_aka_lambda_that_concats_processed_at_the_end(){
+        return null;
+    }
+
+    public Function<Integer, Integer> return_the_previously_created_method(){
+        return null;
+    }
+
+    public String execute_a_function_lambda(Function<Integer, String> integerConverter){
+        return null;
+    }
+
+    public Function<Integer, Integer> create_a_lambda_which_guarantees_the_input_to_be_of_the_same_type_as_the_return_type(){
+        return null;
+    }
+
+    public Predicate<Integer> create_a_lambda_which_returns_true_if_the_integer_is_above_5(){
+        return null;
+    }
+
+    public boolean execute_a_predicate_lambda(Predicate<Integer> alwaysTrue){
+        return false;
+    }
+
+    public Consumer<List<String>> create_a_lambda_which_logs_all_the_entries_without_changing_the_list_itself(){
+        return null;
+    }
+
+    public void execute_a_consumer_lambda(Consumer<List<String>> ssssh_its_a_secret){
+        return;
+    }
+
+    public Supplier<Integer> create_a_lambda_which_returns_an_integer_which_is_always_above_100(){
+        return null;
+    }
+
+    public Double execute_a_supplier_lambda(Supplier<Double> randomNumberGenerator){
+        return null;
+    }
+
+    public Predicate create_a_predicate_which_accepts_primitive_longs_so_no_boxing(){
+        return null;
+    }
+}
---
+++ OCP8 - Lambdas/src/test/java/com/qualogy/training/codelabs/java/LambdasTest.java
@@ -0,0 +1,118 @@
+package com.qualogy.training.codelabs.java;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+public class LambdasTest {
+    private Lambdas test = new Lambdas();
+
+    @Before
+    public void setup(){
+    }
+
+    @Test
+    public void create_a_simple_method_which_doubles_the_integer_value(){
+        assertEquals(new Integer(14), test.create_a_simple_method_which_doubles_the_integer_value(7));
+    }
+
+    @Test
+    public void create_a_anonymous_class_with_a_subtype_of_illegal_argument_exception(){
+        final IllegalArgumentException result = test.create_a_anonymous_class_with_a_subtype_of_illegal_argument_exception();
+        assertNotNull(result);
+        assertNotEquals(IllegalArgumentException.class, result.getClass());
+    }
+
+    @Test
+    public void create_a_short_hand_function_aka_lambda_that_concats_processed_at_the_end(){
+        assertEquals("string_processed", test.create_a_short_hand_function_aka_lambda_that_concats_processed_at_the_end().apply("string_"));
+    }
+
+    @Test
+    public void return_the_previously_created_method(){
+        Function<Integer, Integer> result = test.return_the_previously_created_method();
+        assertNotNull(result);
+        assertEquals(new Integer(14), result.apply(7));
+    }
+
+    public void execute_a_function_lambda(){
+        assertEquals("7", test.execute_a_function_lambda(String::valueOf));
+    }
+
+    @Test
+    public void create_a_function_which_guarantees_the_input_to_be_of_the_same_type_as_the_return_type(){
+        assertTrue(test.create_a_lambda_which_guarantees_the_input_to_be_of_the_same_type_as_the_return_type() instanceof UnaryOperator);
+    }
+
+    @Test
+    public void create_a_lambda_which_returns_true_if_the_integer_is_above_5(){
+        assertTrue(test.create_a_lambda_which_returns_true_if_the_integer_is_above_5().test(6));
+        assertFalse(test.create_a_lambda_which_returns_true_if_the_integer_is_above_5().test(5));
+    }
+
+    @Test
+    public void execute_a_predicate_lambda(){
+        assertTrue(test.execute_a_predicate_lambda((in) -> true));
+    }
+
+    @Test
+    public void create_a_lambda_which_logs_all_the_entries_without_changing_the_list_itself(){
+        PrintStream originalStream = System.out;
+        PrintStream mockStream = mock(PrintStream.class);
+        System.setOut(mockStream);
+
+        final String entry3 = "darkside";
+        final String entry1and2 = "something";
+        List<String> inputList = Arrays.asList(entry1and2, entry1and2, entry3);
+
+        test.create_a_lambda_which_logs_all_the_entries_without_changing_the_list_itself().accept(inputList);
+
+        verify(mockStream, times(3)).println(any(String.class));
+        assertEquals(entry1and2, inputList.get(0));
+        assertEquals(entry1and2, inputList.get(1));
+        assertEquals(entry3, inputList.get(2));
+        assertEquals(3, inputList.size());
+
+        System.setOut(originalStream);
+    }
+
+    @Test
+    public void execute_a_consumer_lambda(){
+        Consumer<List<String>> cons = mock(Consumer.class);
+
+        test.execute_a_consumer_lambda(cons);
+
+        verify(cons).accept(anyList());
+    }
+
+    @Test
+    public void create_a_lambda_which_returns_an_integer_which_is_always_above_100(){
+        Supplier<Integer> result = test.create_a_lambda_which_returns_an_integer_which_is_always_above_100();
+
+        Arrays.stream(new String[5])
+                .forEach((x) -> {assertTrue(result.get() > 100);});
+    }
+
+    @Test
+    public void execute_a_supplier_lambda(){
+        assertNotNull(test.execute_a_supplier_lambda(Math::random));
+    }
+
+    @Test
+    public void create_a_predicate_which_accepts_primitive_longs_so_no_boxing(){
+        assertTrue(test.create_a_predicate_which_accepts_primitive_longs_so_no_boxing() instanceof LongPredicate);
+    }
+
+    @After
+    public void cleanUp(){
+    }
+}
